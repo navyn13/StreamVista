7d441a99281b80873d35756b0aa22c213ac849b2
Naveen <navyn13102003@gmail.com>
2024-01-24 20:46:26 +0530

    login and signup working

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,20 +1,51 @@
-const model= require('../models/index')
+const model = require("../models/index");
 const User = model.User;
-const bcrypt = require('bcrypt');
-const bodyParser = require('body-parser');
+const bcrypt = require("bcrypt");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
 
-exports.signup = async (req, res)=>{
-    const { username, email, password} = req.query;
-    const hashedPassword = bcrypt.hashSync(password, 10)
-    const user = new User({
-        username, 
-        email,
-        password: hashedPassword,
-    })
-    user.save()
-    res.json({ success: true, message: req.query });
-}
 
-exports.login = async(req, res)=>{
-    
-}
+
+exports.signup = async (req, res) => {
+  try {
+    const { username, email, password } = req.query; 
+    const existingUser = await User.findOne({ email: email });
+    if (existingUser) {
+      return res.status(400).json({ success: false, message: "Email is already in use" });
+    }
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    const token = jwt.sign({ username, email }, "secret_key");
+    res.status(200).json({ success: true, token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.query;
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+      return res.status(401).json({ success: false, message: "Invalid Credentials" });
+    }
+    const match = await bcrypt.compare(password, user.password);
+    if (match) {
+      var token = jwt.sign({ email: user.email }, "secret_key");
+      res.json({ success: true, token, message: "Correct Credentials" });
+    } else {
+      res.status(401).json({ success: false, message: "Invalid Credentials" });
+    }
+  } catch (err) {
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
